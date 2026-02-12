afdb963756cbae60efaacbaf69768e08d332030e
prathamesh <prathameshwarekar21@gmail.com>
2026-02-12 12:09:06 +0530

    add port

--- src/config/db.js
+++ src/config/db.js
@@ -4,6 +4,7 @@ const mysql = require("mysql2/promise");
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
+  port:process.env.PORT,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
     ssl: {
